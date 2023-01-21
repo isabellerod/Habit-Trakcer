7be29377da4119ffda345c7c05c97f07f48f88f8
isabellerod <isabeller29@hotmail.com>
2023-01-21 12:09:37 -0300

    save local data

--- Habit-tracker/script.js
+++ Habit-tracker/script.js
@@ -1,8 +1,26 @@
 const tracker = document.querySelector('#tracker');
-const library = new NLWSetup(tracker);
-const data = {
-    run: ['01-15', '01-16', '01-17']
+const nlwSetup = new NLWSetup(tracker);
+const button = document.querySelector("header button");
+
+button.addEventListener('click', register);
+tracker.addEventListener('change', save);
+
+function register(){
+    const day = new Date().toLocaleDateString('en').slice(0, -5);
+    const dayExists = nlwSetup.dayExists(day);
+    if (dayExists){
+        alert('This date already exists');
+        return;
+    }
+    alert('Day added');
+    nlwSetup.addDay(day);
 }
 
-library.setData(data);
-library.load();
+function save(){
+    localStorage.setItem('HabitTracker', JSON.stringify(nlwSetup.data));
+    console.log(nlwSetup.data);
+}
+
+const data = JSON.parse(localStorage.getItem('HabitTracker')) || {};
+nlwSetup.setData(data);
+nlwSetup.load();
